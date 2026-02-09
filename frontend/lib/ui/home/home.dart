84808c1c22bb03221ef488daddad3394b861ad00
phatsachon99 <phatsachon.bun@dome.tu.ac.th>
2026-02-09 16:32:32 +0700

    Update home.dart

--- frontend/lib/ui/home/home.dart
+++ frontend/lib/ui/home/home.dart
@@ -7,9 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return 
     Scaffold(
-      appBar: AppBar(
-        title: Text('Ghost ปะคะ?'),
-      ),
+
       backgroundColor: Colors.black, // พื้นหลังรอบ ๆ
       body: Center(
         child: LayoutBuilder(

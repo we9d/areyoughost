c3ac37b50d411170b6065aa3a66312d58ad57d92
phatsachon09 <phatsachon25488@gmail.com>
2026-03-08 18:25:50 +0700

    Reject ,Accept button, Mailnoti Icon
    Khaw

--- frontend/lib/ui/widgets/icons/my_flutter_app_icons.dart
+++ frontend/lib/ui/widgets/icons/my_flutter_app_icons.dart
@@ -1,8 +1,8 @@
-class MyFlutterApp {
-  MyFlutterApp._();
+// class MyFlutterApp {
+//   MyFlutterApp._();
 
-  static const _fontFamily = 'MyFlutterApp';
+//   static const _fontFamily = 'MyFlutterApp';
 
-  static const IconData skill =
-      IconData(0xe800, fontFamily: _fontFamily);
-}
+//   static const IconData skill =
+//       IconData(0xe800, fontFamily: _fontFamily);
+// }
